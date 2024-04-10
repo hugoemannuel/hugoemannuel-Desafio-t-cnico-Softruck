3daa37e15a676a78740133bd020030199f26b03c
hugo emannuel <hugoemannuel53@gmail.com>
2024-04-10 19:12:48 -0300

    Feature: ⚙️ Adding trajectory

--- App.js
+++ App.js
@@ -1,23 +1,54 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import MapView, { Marker } from "react-native-maps";
+import Data from "./data/frontend_data_gps.json";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const { courses, vehicle } = Data;
+
+  const [spritePositionIndex, setSpritePositionIndex] = useState(0);
+
+  const [spritePosition, setSpritePosition] = useState({
+    latitude: courses[0].gps[0].latitude,
+    longitude: courses[0].gps[0].longitude,
+  });
+
+  const handleUpdateCord = () => {
+    const nextIndex = spritePositionIndex + 1;
+    if (nextIndex < courses[0].gps.length) {
+      const nextCoordinates = courses[0].gps[nextIndex];
+      setSpritePositionIndex(nextIndex);
+      setSpritePosition({
+        latitude: nextCoordinates.latitude,
+        longitude: nextCoordinates.longitude,
+      });
+    }
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      handleUpdateCord();
+    }, 1500);
+
+    return () => clearInterval(interval);
+  }, [spritePosition]);
+
   return (
     <View style={styles.container}>
       <MapView
         style={styles.map}
-        region={{
-          latitude: -46.28054,
-          longitude: -23.963214,
+        initialRegion={{
+          latitude: spritePosition.latitude,
+          longitude: spritePosition.longitude,
           latitudeDelta: 0.015,
           longitudeDelta: 0.0121,
         }}
       >
         <Marker
+          icon={{ uri: vehicle.picture.address}}
           coordinate={{
-            latitude: 37.78825,
-            longitude: -122.4324,
+            latitude: spritePosition.latitude,
+            longitude: spritePosition.longitude,
           }}
         />
       </MapView>
@@ -28,6 +59,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
